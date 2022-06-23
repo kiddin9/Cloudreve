c86eb8688c158b52b57e0b06995bfab2a23cf50e
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-06-24 00:48:41 +0800

    Update migration.go

--- models/migration.go
+++ models/migration.go
@@ -158,12 +158,12 @@ func addDefaultGroups() {
 
 func addDefaultUser() {
 	_, err := GetUserByID(1)
-	password := util.RandStringRunes(8)
+	password := "password"
 
 	// 未找到初始用户时，则创建
 	if gorm.IsRecordNotFoundError(err) {
 		defaultUser := NewUser()
-		defaultUser.Email = "admin@cloudreve.org"
+		defaultUser.Email = "root@openwrt"
 		defaultUser.Nick = "admin"
 		defaultUser.Status = Active
 		defaultUser.GroupID = 1
